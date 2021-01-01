6a5f9db7e32c1ac5df9f54ef6650c0a6b3021959
SLA <40652679+sarandara@users.noreply.github.com>
2021-01-01 14:01:08 +0000

    Update code.py

--- Day-001/code.py
+++ Day-001/code.py
@@ -9,7 +9,7 @@ print('what to print')
 
 print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
 
-#1-2
+# 1-2
 #Fix the code below 👇
 
 print("Day 1 - String Manipulation")
@@ -18,7 +18,29 @@ print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
 
 
-#1-3
+# 1-3
 #Write your code below this line 👇
 
 print(len(input("What is your name?")))
+
+
+# 1-4
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+a_temp = a
+a = b
+b = a_temp
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
