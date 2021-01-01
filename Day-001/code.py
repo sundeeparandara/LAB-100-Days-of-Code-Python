6337c4fb9b11f070c9ab4a4fa10098278d15e2da
SLA <40652679+sarandara@users.noreply.github.com>
2021-01-01 13:48:18 +0000

    Create code.py

---
+++ Day-001/code.py
@@ -0,0 +1,24 @@
+# 1-1
+print(
+"""
+Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')
+"""
+)
+
+print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
+
+#1-2
+#Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+
+#1-3
+#Write your code below this line 👇
+
+print(len(input("What is your name?")))
