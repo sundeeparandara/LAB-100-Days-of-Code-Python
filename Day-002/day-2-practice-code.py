b40d30e487d152031cfa17b1a3b150f11cd41f41
SLA <40652679+sarandara@users.noreply.github.com>
2021-01-03 16:54:24 +0000

    Update day-2-practice-code.py

--- Day-002/day-2-practice-code.py
+++ Day-002/day-2-practice-code.py
@@ -8,3 +8,33 @@ two_digit_number = input("Type a two digit number: ")
 print(int(two_digit_number[0])+int(two_digit_number[1]))
 
 
+####################################################
+
+
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+bmi = int(float(weight) / float(height)**2)
+print(bmi)
+
+###########################################################
+
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+age = int(age)
+
+yr = 90 - age
+
+dr = yr * 365
+wr = yr * 52
+mr = yr * 12
+
+print(f"You have {dr} days, {wr} weeks and {mr} months left.")
