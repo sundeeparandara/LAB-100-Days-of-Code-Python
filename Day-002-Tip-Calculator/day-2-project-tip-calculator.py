dd6c5d6844fee01cfd1c6352250f4fd279991c52
SLA <40652679+sarandara@users.noreply.github.com>
2021-01-04 19:23:11 +0000

    Update day-2-project-tip-calculator.py

--- Day-002-Tip-Calculator/day-2-project-tip-calculator.py
+++ Day-002-Tip-Calculator/day-2-project-tip-calculator.py
@@ -4,22 +4,19 @@
 #Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
 #HINT 1: https://www.google.com/search?q=how+to+round+number+to+2+decimal+places+python&oq=how+to+round+number+to+2+decimal
 #HINT 2: https://www.kite.com/python/answers/how-to-limit-a-float-to-two-decimal-places-in-python
-####################################################################################
+################################################################
+
 import numpy as np
 
 print("Welcome to the tip calculator.")
 
-total_bill = input("What was the total bill? $")
-total_bill =  float(total_bill)
+total_bill = float(input("What was the total bill? $"))
 
-pct_tip = input("What percentage tip would you like to give? 10, 12, 15? ")
-pct_tip = float(pct_tip)
-
-people = input("How many people to split the bill?")
-people = int(people)
+pct_tip = float(input("What percentage tip would you like to give? 10, 12, 15? "))
+people = int(input("How many people to split the bill?"))
 
 total_bill_with_tip = total_bill*(1+pct_tip/100)
 
-cost_per_person = np.round(total_bill_with_tip/people,decimals=2)
+cost_per_person = "{:.2f}".format(np.round(total_bill_with_tip/people,decimals=2))
 
 print(f"Each person should pay: ${cost_per_person}")
