b999e401e8595961fb44ee5bf9ed1d703fadf4e9
SLA <40652679+sarandara@users.noreply.github.com>
2021-01-05 19:45:27 +0000

    Update day-3-practice-code.py

--- Day-003-Treasure-Island/day-3-practice-code.py
+++ Day-003-Treasure-Island/day-3-practice-code.py
@@ -5,3 +5,41 @@ if height > 120:
   print("Can ride")
 else:
   print("Can't ride")
+
+
+###############################################
+
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+if ((number % 2) == 0):
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+
+
+
+############################
+
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+
+BMI = int(round(weight/height**2,0))
+
+if BMI < 18.5:
+  print(f"Your BMI is {BMI}, you are underweight.")
+elif BMI < 25:
+  print(f"Your BMI is {BMI}, you have a normal weight.")
+elif BMI <30:
+  print(f"Your BMI is {BMI}, you are slightly overweight.")
+elif BMI <35:
+  print(f"Your BMI is {BMI}, you are obese.")
+else:
+  print(f"Your BMI is {BMI}, you are clinically obese.")
