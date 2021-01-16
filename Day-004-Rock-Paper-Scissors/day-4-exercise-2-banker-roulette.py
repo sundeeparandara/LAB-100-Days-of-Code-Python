b681266372c192249998beb72a12659989e1da3e
SLA <40652679+sarandara@users.noreply.github.com>
2021-01-16 12:23:21 +0000

    Create day-4-exercise-2-banker-roulette.py

---
+++ Day-004-Rock-Paper-Scissors/day-4-exercise-2-banker-roulette.py
@@ -0,0 +1,14 @@
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+import random
+
+
+buyer = names[random.randint(0,len(names)-1)]
+
+print(f"{buyer} is going to buy the meal today.")
+
