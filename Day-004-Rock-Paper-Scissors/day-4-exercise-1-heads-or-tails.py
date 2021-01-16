6f2c6481e767078a1380860dff8b7cb417914983
SLA <40652679+sarandara@users.noreply.github.com>
2021-01-16 11:53:42 +0000

    Create day-4-exercise-1-heads-or-tails.py

---
+++ Day-004-Rock-Paper-Scissors/day-4-exercise-1-heads-or-tails.py
@@ -0,0 +1,11 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random
+
+val = random.randint(0,1)
+##print(val)
+
+if val == 1:
+  print('Heads')
+else:
+  print('Tails')
