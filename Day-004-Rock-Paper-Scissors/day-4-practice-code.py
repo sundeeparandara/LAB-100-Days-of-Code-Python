9f9cfb3c346b3648ec88bf70c61875cbbe364f9a
SLA <40652679+sarandara@users.noreply.github.com>
2021-01-16 11:48:03 +0000

    update

--- Day-004-Rock-Paper-Scissors/day-4-practice-code.py
+++ Day-004-Rock-Paper-Scissors/day-4-practice-code.py
@@ -2,4 +2,19 @@ import random
 
 print(random.randint(1,3))
 
-###################################################
+############
+
+import my_module
+
+print(my_module.pi)
+
+
+##########
+
+print(random.random())
+
+
+###########
+
+print(random.uniform(0,5))
+print(random.random()*5)
