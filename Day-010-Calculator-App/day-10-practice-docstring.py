cc781a21d1f8c11f810fcda98a69559022a3ea36
SLA <40652679+sarandara@users.noreply.github.com>
2021-01-29 19:23:42 +0000

    Create day-10-practice-docstring.py

---
+++ Day-010-Calculator-App/day-10-practice-docstring.py
@@ -0,0 +1,10 @@
+def format_name2(first_name, last_name):
+  """
+  This is docstring.
+  Take me to your leader.
+  """
+  full_name = (first_name + " " + last_name).title()
+
+  return full_name
+
+print(format_name2('suNdeeP','ARANdara'))
