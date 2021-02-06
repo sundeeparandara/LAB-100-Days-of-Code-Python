fb036aa02d72617300e604edce380dfff2271422
SLA <40652679+sarandara@users.noreply.github.com>
2021-02-06 15:22:43 +0000

    Update day-12-project-number-guessing-game.py

--- Day-012-Scope/day-12-project-number-guessing-game.py
+++ Day-012-Scope/day-12-project-number-guessing-game.py
@@ -1,29 +1,15 @@
-#Number Guessing Game Objectives:
+##########################
+## NUMBER GUESSING GAME###
+##########################
 
-# Include an ASCII art logo.
-# Allow the player to submit a guess for a number between 1 and 100.
-# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
-# If they got the answer correct, show the actual answer to the player.
-# Track the number of turns remaining.
-# If they run out of turns, provide feedback to the player. 
-# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
-
+#Libaries
 import random
 
+#Constants
+EASY_TURNS = 10
+HARD_TURNS = 5
 
-print("Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.")
-number_actual = random.randint(1,100)
-
-difficult_level = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
-if difficult_level == 'easy':
-  number_of_attempts = 10
-elif difficult_level == 'hard':
-  number_of_attempts = 5
-else:
-  print("You didn't follow the instructions.\nYou get one shot."
-  )
-  number_of_attempts = 1
-
+#Functions
 def assess_guess(guess,actual):
   state = ""
   if guess == actual:
@@ -37,6 +23,21 @@ def assess_guess(guess,actual):
     state = "LOSE"
   return state
 
+########################################################
+
+print("Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.")
+number_actual = random.randint(1,100)
+
+difficult_level = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+if difficult_level == 'easy':
+  number_of_attempts = EASY_TURNS
+elif difficult_level == 'hard':
+  number_of_attempts = HARD_TURNS
+else:
+  print("You didn't follow the instructions.\nYou get one shot."
+  )
+  number_of_attempts = 1
+
 while not number_of_attempts == 0:
   print(f"You have {number_of_attempts} attempt(s) remaining to guess the number.")
   number_of_attempts -= 1
@@ -49,7 +50,6 @@ while not number_of_attempts == 0:
     print("You've run out of guesses.")
     print(f"The correct answer is: {number_actual}")
     print("DEFEAT!\nGAME OVER")
-
     break
   else:
     print("Guess again.")
