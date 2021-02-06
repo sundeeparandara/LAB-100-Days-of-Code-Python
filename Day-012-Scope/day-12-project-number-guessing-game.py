a648d496f08b134468d3e36a7c09a241b20e259a
SLA <40652679+sarandara@users.noreply.github.com>
2021-02-06 15:19:31 +0000

    Update day-12-project-number-guessing-game.py

--- Day-012-Scope/day-12-project-number-guessing-game.py
+++ Day-012-Scope/day-12-project-number-guessing-game.py
@@ -1,4 +1,12 @@
-#NUMBER GUESSING GAME
+#Number Guessing Game Objectives:
+
+# Include an ASCII art logo.
+# Allow the player to submit a guess for a number between 1 and 100.
+# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
+# If they got the answer correct, show the actual answer to the player.
+# Track the number of turns remaining.
+# If they run out of turns, provide feedback to the player. 
+# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
 
 import random
 
@@ -19,7 +27,7 @@ else:
 def assess_guess(guess,actual):
   state = ""
   if guess == actual:
-    print("You guessed right!\nVICTORY!")
+    print("You guessed right!")
     state = "WIN"
   elif guess < actual:
     print("Too low.")
@@ -35,12 +43,12 @@ while not number_of_attempts == 0:
   number_guess = int(input("Make a guess: "))
   state = assess_guess(number_guess, number_actual)
   if state == "WIN":
-    print("GAME OVER")
+    print("VICTORY!\nGAME OVER")
     break
   elif state == "LOSE" and number_of_attempts == 0:
     print("You've run out of guesses.")
     print(f"The correct answer is: {number_actual}")
-    print("DEFEAT\nGAME OVER")
+    print("DEFEAT!\nGAME OVER")
 
     break
   else:
