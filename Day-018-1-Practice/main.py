7beb81117950783f8f991fef1afc6e31361ec91e
SLA <40652679+sarandara@users.noreply.github.com>
2021-03-25 05:43:23 +0000

    update

---
+++ Day-018-1-Practice/main.py
@@ -0,0 +1,30 @@
+#https://docs.python.org/3/library/turtle.html
+from turtle import Turtle, Screen
+
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("red")
+
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
+
---
+++ Day-018-2-Turtle-Challenge-1/main.py
@@ -0,0 +1,9 @@
+from turtle import Turtle
+
+raphael = Turtle()
+raphael.shape("turtle")
+raphael.color("red")
+
+for _ in range(4):
+    raphael.forward(100)
+    raphael.right(90)
