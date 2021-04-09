6b57f5373114dde43bf41ac48703f2d18ad72f7d
SLA <40652679+sarandara@users.noreply.github.com>
2021-04-09 12:23:09 +0100

    Update main.py

--- Day-20-21-Snake-Game/main.py
+++ Day-20-21-Snake-Game/main.py
@@ -5,18 +5,45 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
-square = Turtle("square")
-square.fillcolor("white")
-
 snake_length = 3
 x_pos = 0
+x_head = 0
+y_pos = 0
+block_increment = 20
+snake = []
 
-for i in range(snake_length):
+for _ in range(snake_length):
     square = Turtle("square")
     square.penup()
+    square.speed("fastest")
+    square.goto(x=x_pos, y=0)
     square.color("white")
-    square.goto(x=x_pos,y=0)
+    snake.append(square)
     x_pos -= 20
+
+
+for i in range(10):
+    x_head = block_increment*i
+    x_pos = x_head
+    j = 1
+    print(f"round {i}")
+    for square in snake:
+        square.goto(x_pos,y=0)
+        position = square.pos()
+        print(f"{j} : {position}")
+        j +=1
+        x_pos -= 20
+
+# for i in range(10):
+#     x_head = block_increment*i
+#     x_pos = x_head
+#     print(x_pos)
+#     for _ in range(snake_length):
+#         square = Turtle("square")
+#         square.penup()
+#         square.color("white")
+#         square.goto(x=x_pos,y=0)
+#         x_pos -= 20
 
 
 
